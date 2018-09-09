fb444042300fd9fb3d733432d028f642f5687c20
Ganesh Kanna <mail2sgk@gmail.com>
2018-09-09 20:40:05 +0530

    modified access modifier

--- app/src/main/java/com/gk/connection/ConnectionUtil.kt
+++ app/src/main/java/com/gk/connection/ConnectionUtil.kt
@@ -4,21 +4,24 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 
-object ConnectionUtil {
+open class ConnectionUtil {
 
-    fun isNetworkAvailable(context: Context): Boolean {
-        try {
-            val connectivity = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (connectivity != null && connectivity.allNetworkInfo != null) {
-                for (anInfo in connectivity.allNetworkInfo)
-                    if (anInfo.state == NetworkInfo.State.CONNECTED)
-                        return true
+    companion object {
+        public fun isNetworkAvailable(context: Context): Boolean {
+            try {
+                val connectivity = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                if (connectivity != null && connectivity.allNetworkInfo != null) {
+                    for (anInfo in connectivity.allNetworkInfo)
+                        if (anInfo.state == NetworkInfo.State.CONNECTED)
+                            return true
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return false
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
+
             return false
         }
-
-        return false
     }
+
 }
